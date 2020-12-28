4130be91b1136369a2b5d47e7fa48ff5e2be81b4
Eli Parkhurst <eli.parkhurst@cakeinsure.com>
2020-12-28 10:58:15 -0700

    15-1

---
+++ day15/part1.py
@@ -0,0 +1,28 @@
+f = open('input.txt', 'r')
+input_data = f.read().strip().split(',')
+f.close()
+
+
+def get_latest_index(li, x, current):
+    for i in reversed(range(len(li) -1)):
+        if li[i] == x:
+            return current - i - 1
+    return 0
+
+def run_program(data):
+    running_list = data.copy()
+    running_list = [int(num) for num in running_list]
+    for i in range(len(data), 2020):
+        last =  running_list[-1]
+        next = get_latest_index(running_list, last, i)
+        running_list.append(next)
+    return running_list[-1]
+
+
+def main():
+    final = run_program(input_data)
+    print(final)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day15/part2.py
@@ -0,0 +1,16 @@
+f = open('input.txt', 'r')
+input_data = f.read().strip().split(',')
+f.close()
+
+
+def run_program(data):
+    return 'final'
+
+
+def main():
+    final = run_program(input_data)
+    print(final)
+
+
+if __name__ == "__main__":
+    main()
