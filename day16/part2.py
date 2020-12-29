b8ba1898f0fa3968ec92e935b6f6521723170d1b
eparkhurst <eli.parkhurst@gmail.com>
2020-12-28 19:35:10 -0700

    16-2 done. Feels ugly and inefficient but it works

--- day16/part2.py
+++ day16/part2.py
@@ -13,27 +13,42 @@ def v_factory(rule):
     return temp
 
 
-def get_funcs(rules):
+def get_funcs():
     verifiers = []
-    rules = rules.split('\n')
-    for rule in rules:
+    split_rules = rules.split('\n')
+    for rule in split_rules:
         verifiers.append(v_factory(rule))
     return verifiers
 
 def get_positions(verifiers, valid_tickets):
+    mold = [True] * len(verifiers)
+    matrix = []
     for i in range(len(verifiers)):
+        matrix.append(mold.copy())
         for ticket in valid_tickets:
-            if verifiers[i](ticket[i]) is False
+            for j in range(len(ticket)):
+                if verifiers[i](ticket[j]) is False:
+                    matrix[i][j] = False
+    split_rules = rules.split('\n')
+    split_rules = [rule.split(':')[0] for rule in split_rules]
 
-
-
+    positions = {}
+    done = 0
+    while done < len(matrix[0]):
+        for i in range(len(matrix)):
+            if matrix[i].count(True) == 1:
+                index = matrix[i].index(True)
+                positions[split_rules[i]] = index
+                done += 1
+                for line in matrix:
+                    line[index] = False
+    return positions
 
 def run_program():
-    verifiers = get_funcs(rules)
+    verifiers = get_funcs()
     tickets = nearby_tickets.split('\n')[1:]
     tickets = [t.split(',') for t in tickets]
 
-    print(len(tickets))
     valid_tickets = []
     for ticket in tickets:
         valid_ticket = True
@@ -46,13 +61,22 @@ def run_program():
                 valid_ticket = False
         if valid_ticket is True:
             valid_tickets.append(ticket)
-    print(len(valid_tickets))
-    return 'hey'
+    positions = get_positions(verifiers, valid_tickets)
+
+    my_ticket = your_ticket.split('\n')[1]
+    my_ticket = my_ticket.split(',')
+    total = 1
+    for field in positions.keys():
+        if 'departure' in field:
+            total *= int(my_ticket[positions[field]])
+    return total
 
 
 def main():
-    run_program()
-
+    final = run_program()
+    print(final)
 
 if __name__ == "__main__":
     main()
+
+# 762 is too low
